fecb8bb83f43d3f398189b811a0d1feefaa9a868
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-30 18:33:40 -0300

    chore: add .yarnrc.yml and update Vite config to load environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import webExtension from '@samrum/vite-plugin-web-extension';
 import path from 'path';
 import { getManifest } from './src/manifest';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
   return {
     plugins: [
       react(),
       webExtension({
-        manifest: getManifest(),
+        manifest: getManifest(env),
       }),
     ],
     resolve: {
@@ -18,5 +20,8 @@ export default defineConfig(() => {
         '~': path.resolve(__dirname, './src'),
       },
     },
+    define: {
+      'process.env.VITE_SUMMARIZATION_API_URL': JSON.stringify(env.VITE_SUMMARIZATION_API_URL),
+    }
   };
 });
