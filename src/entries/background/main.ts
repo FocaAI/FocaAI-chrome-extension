77706a7ff3e87c1ab8f74981f5b90af06cbe0e83
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-30 18:33:57 -0300

    feat: implement side panel for Google and add sidebar component

--- src/entries/background/main.ts
+++ src/entries/background/main.ts
@@ -3,3 +3,25 @@ import browser from "webextension-polyfill";
 browser.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 });
+
+const GOOGLE_ORIGIN = 'https://www.google.com';
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel on google.com
+  if (url.origin === GOOGLE_ORIGIN) {
+    alert('Google detected');
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: 'src/entries/sidebar/index.html',
+      enabled: true
+    });
+  } else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      enabled: false
+    });
+  }
+});
--- src/manifest.ts
+++ src/manifest.ts
@@ -1,8 +1,33 @@
 import pkg from "../package.json";
-
-const manifest = {
-  action: {
-    default_icon: {
+type ManifestWithSidePanel = chrome.runtime.ManifestV3 & { side_panel: { default_path: string } };
+export function getManifest(env: Record<string, string>): ManifestWithSidePanel {
+  const manifest = {
+    action: {
+      default_icon: {
+        36: "icons/36.png",
+        48: "icons/48.png",
+        72: "icons/72.png",
+        96: "icons/96.png",
+        144: "icons/144.png",
+        192: "icons/192.png",
+        512: "icons/512.png",
+      },
+      default_popup: "src/entries/popup/index.html",
+    },
+    background: {
+      service_worker: "src/entries/background/main.ts",
+    },
+    side_panel: {
+      default_path: "src/entries/sidebar/index.html",
+    },
+    content_scripts: [
+      {
+        js: ["src/entries/contentScript/primary/main.tsx"],
+        matches: ["*://*/*"],
+      },
+    ],
+    host_permissions: ["*://*/*"],
+    icons: {
       36: "icons/36.png",
       48: "icons/48.png",
       72: "icons/72.png",
@@ -11,41 +36,19 @@ const manifest = {
       192: "icons/192.png",
       512: "icons/512.png",
     },
-    default_popup: "src/entries/popup/index.html",
-  },
-  background: {
-    service_worker: "src/entries/background/main.ts",
-  },
-  content_scripts: [
-    {
-      js: ["src/entries/contentScript/primary/main.tsx"],
-      matches: ["*://*/*"],
+    options_ui: {
+      page: "src/entries/options/index.html",
+      open_in_tab: true,
     },
-  ],
-  host_permissions: ["*://*/*"],
-  icons: {
-    36: "icons/36.png",
-    48: "icons/48.png",
-    72: "icons/72.png",
-    96: "icons/96.png",
-    144: "icons/144.png",
-    192: "icons/192.png",
-    512: "icons/512.png",
-  },
-  options_ui: {
-    page: "src/entries/options/index.html",
-    open_in_tab: true,
-  },
-  trial_tokens: [import.meta.env.VITE_SUMMARIZATION_API_URL || ""]
-};
-
-export function getManifest(): chrome.runtime.ManifestV3 {
+    trial_tokens: [env.VITE_SUMMARIZATION_API_URL || ""]
+  };
   return {
     author: pkg.author.name,
     description: pkg.description,
     name: pkg.displayName ?? pkg.name,
     version: pkg.version,
     manifest_version: 3,
+    permissions: ["contextMenus", "sidePanel"],
     ...manifest,
   };
 }
