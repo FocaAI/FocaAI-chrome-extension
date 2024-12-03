b379327e937f28348765e1cdcce478bb78b199a9
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-12-03 20:13:10 -0300

    styles: import index css to use tailwind configuration properly

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import logo from '@/assets/logo.svg';
-
+import '@/index.css'
 function PageContent(props: { children: React.ReactNode }) {
   const imageUrl = new URL(logo, import.meta.url).href;
 
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex flex-col items-center gap-y-2 bg-gray-50 px-2 py-4 text-center">
+    <div className="flex flex-col items-center gap-y-2 px-2 py-4 text-center">
       <img src={imageUrl} width={50} alt="" />
       <h1>{props.children}</h1>
       <button
         type="button"
-        className="rounded-md border border-gray-200 bg-white px-4 py-2"
+        className="rounded-md border border-gray-200 px-4 py-2"
         onClick={() => setCount((count) => count + 1)}
       >
         Count: {count}
